98fbf53dcb570a08b42c477b7233bf2538dba57e
koltmcbride <koltafrickenfer@gmail.com>
2016-05-04 18:53:47 -0700

    Added files via upload

---
+++ remote-object.ps1
@@ -0,0 +1,12 @@
+﻿function remote-object{
+
+param( $computername, $scriptblock, $cred )
+
+$session = New-PSSession  -ComputerName $computername -Credential $cred
+$rso = Invoke-Command -session $session -ScriptBlock $scriptblock
+ 
+
+$rso | %{$_.PSObject.Properties.Remove('PSComputerName')}
+$rso
+
+}
