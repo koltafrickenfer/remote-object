c717994006dffd0d3334303fc1f0fe4084edf041
koltafrickenfer <koltafrickenfer@gmail.com>
2017-03-23 11:10:23 -0700

    Update remote-object.ps1

--- remote-object.ps1
+++ remote-object.ps1
@@ -5,7 +5,7 @@ param( $computername, $scriptblock, $cred )
 $session = New-PSSession  -ComputerName $computername -Credential $cred
 $rso = Invoke-Command -session $session -ScriptBlock $scriptblock
  
-
+# removes propetie added on by the Invoke-command cmdlet.
 $rso | %{$_.PSObject.Properties.Remove('PSComputerName')}
 $rso
 
